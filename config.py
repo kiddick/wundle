fb3f8daefee096644ac8242094a0fe63b0528cf4
unknown <kiddick@users.noreply.github.com>
2015-12-15 16:43:21 +0300

    Add background debug thread.

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
+import platform
 import yaml
 
-__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 with open(os.path.join(__location__, 'settings.yaml')) as yaml_settings:
     settings = yaml.load(yaml_settings)
@@ -10,9 +12,11 @@ with open(os.path.join(__location__, 'settings.yaml')) as yaml_settings:
 class Config(object):
     DEBUG = False
     TESTING = False
+    BOT_DEBUG = False
     BOT_TOKEN = settings['bot_token']
 
 
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
+    BOT_DEBUG = 'Win' in platform.system()
--- wundle.py
+++ wundle.py
@@ -1,4 +1,5 @@
 import os
+from threading import Thread
 
 import telegram
 from flask import Flask, request
@@ -8,8 +9,6 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
-
-DEBUG = False
 
 
 global bot
@@ -32,14 +31,14 @@ def webhook_handler():
 
 @app.route('/set_webhook', methods=['GET', 'POST'])
 def set_webhook():
-    with open(os.path.join(__location__, 'ngrok.host'), 'r') as nh:
-        webhook_url = nh.read()
-    print webhook_url
+    with open(os.path.join(__location__, 'ngrok.host'), 'r') as ngrok_host:
+        webhook_url = ngrok_host.read()
     s = bot.setWebhook(webhook_url='https://{}/ya'.format(webhook_url))
     if s:
         return "webhook setup ok"
     else:
         return "webhook setup failed"
+
 
 def unset_webhook():
     bot.setWebhook(webhook_url=None)
@@ -57,10 +56,13 @@ def get_last_update_id():
 
 
 def get_updates():
+    unset_webhook()
     last_update_id = get_last_update_id()
     while True:
         if last_update_id:
             for update in bot.getUpdates(offset=last_update_id, timeout=10):
+                with app.test_request_context('/ya', method='POST', data=update.to_json()):
+                    app.dispatch_request()
                 text = update.message.text
                 chat_id = update.message.chat_id
                 update_id = update.update_id
@@ -73,13 +75,19 @@ def get_updates():
         else:
             last_update_id = get_last_update_id()
 
-if not DEBUG:
+if not app.config['BOT_TOKEN']:
     set_webhook()
 
-if __name__ == '__main__':
-    if DEBUG:
+
+def main():
+    if app.config['BOT_TOKEN']:
         unset_webhook()
-        get_updates()
+        updates_thread = Thread(target=get_updates, args=())
+        updates_thread.setDaemon(True)
+        updates_thread.start()
+        app.run(host='0.0.0.0', use_reloader=False)
     else:
         app.run(host='0.0.0.0')
 
+if __name__ == '__main__':
+    main()
